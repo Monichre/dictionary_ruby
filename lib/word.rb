82eaaf2ef09bb1494ca82eeb60aef9dc20c3ce3c
Liam Ellis <liamhellis@gmail.com>
2016-07-08 12:19:32 -0700

    unit tests passing for word: clear, save, add definitions, get definition

--- lib/word.rb
+++ lib/word.rb
@@ -1,11 +1,9 @@
 class Word
   attr_reader(:the_word_itself, :id, :definitions)
   @@words = []
-  def initialize(the_word_itself, definition)
+  def initialize(the_word_itself)
     @the_word_itself = the_word_itself
-    @definition = definition
     @definitions = []
-    @definitions << @definition
     @id = @@words.length + 1
   end
 
@@ -13,8 +11,16 @@ class Word
     @@words << self
   end
 
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+
   def add_definition(definition)
-    @@definitions << definition
+    @definitions << definition
+  end
+
+  define_singleton_method(:all) do
+    @@words
   end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,10 +2,40 @@ require('rspec')
 require('word')
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
   describe('#definitions') do
     it('returns an array of all definitions') do
-      test_word = Word.new('beholden', 'owing thanks')
+      test_word = Word.new('beholden')
+      test_word.add_definition('owing thanks')
       expect(test_word.definitions()).to(eq(['owing thanks']))
+    end
+  end
+
+  describe('.all') do
+    it('returns an array of all words') do
+      test_word = Word.new('beholden')
+      test_word.save()
+      test_word2 = Word.new('inane')
+      test_word2.save()
+      expect(Word.all()).to(eq([test_word, test_word2]))
+    end
+  end
+
+  describe('#save') do
+    it('saves word to an array of all words') do
+      test_word = Word.new('beholden')
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved words") do
+      Word.new("wash the lion").save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
     end
   end
 end
