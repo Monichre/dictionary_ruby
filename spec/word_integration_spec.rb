73aed2d9bc4dcfb166d0598f729963293aab46bf
Liam Ellis <liamhellis@gmail.com>
2016-07-08 14:03:28 -0700

    full integration specs passing for each route

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,17 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the index path', {:type => :feature}) do
+  it('lists all word entries and displays a form for new words') do
+    visit('/')
+    fill_in('word', :with =>'beholden')
+    fill_in('definition', :with =>'to owe thanks')
+    click_button('Add this entry')
+    expect(page).to have_content('Thank you! Your entry is being processed')
+    click_link('Home')
+    expect(page).to have_content('beholden')
+    click_link('beholden')
+    expect(page).to have_content('to owe thanks')
+  end
+end
